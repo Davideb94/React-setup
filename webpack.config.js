151cbc99d14b0db4bf1cf0dbed7634ad283854ad
Davide Brunetti <davide.brunetti@edu.unito.it>
2018-06-30 17:32:03 +0200

    :bug: Removed bug from webpack config file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const path = require('path');
 
-const path = require('path');
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
     template: './public/index.html',
