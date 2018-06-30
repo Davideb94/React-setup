70046a9f75a9f7c287cec0a59cfb414006262737
Davide Brunetti <davide.brunetti@edu.unito.it>
2018-06-30 17:11:17 +0200

    :sparkles: Added entry point file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
+
 module.exports = {
-    entry: './client/index.js',
+    entry: './src/index.js',
     output: {
         path: path.resolve('dist'),
         filename: 'index_bundle.js'
